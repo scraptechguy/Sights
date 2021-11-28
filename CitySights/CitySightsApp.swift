6476d6b59f6fc32c20154404afa2445b228f5f5f
scraptechguy <rostislav.bro@gmail.com>
2021-11-28 13:38:31 +0100

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Rostislav Brož on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CitySights
+//
+//  Created by Rostislav Brož on 11/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
